587852e1bea14178e0be859b2bbdaf8387f14f29
breismanC <breisman.chalaca@udea.edu.co>
2023-08-11 17:21:44 -0500

    Validations without middlewares

--- back/src/routes/users.routes.js
+++ back/src/routes/users.routes.js
@@ -9,24 +9,8 @@ const {
   deleteUser,
 } = require("../controllers/users.controllers");
 
-router.route("/")
-  .get(getUsers)
-  .post(postUser);
+router.route("/").get(getUsers).post(postUser);
 
-router
-  .route("/:id")
-  .get(getUserById)
-  .put(updateUser)
-  .delete(deleteUser);
-
-// router.get("/", getUsers);
-
-// router.get("/:id", getUserById);
-
-// router.post("/", postUser);
-
-// router.put("/:id", updateUser);
-
-// router.delete("/:id", deleteUser);
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
 
 module.exports = router;
